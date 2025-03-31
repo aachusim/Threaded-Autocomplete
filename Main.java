bf86128a7aa8ba6dcd9c23fdfd9a7f6b10bd257e
Andrew <aachusimwarp@gmail.com>
2025-03-31 10:09:21 -0600

    Mod choice

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+Andrew Achusim
+03/27/2025
+Purpose: To complete the four TODOs in SuggestionGUI.java then make AT LEAST ONE additional
+modification of your choice (STOP ON EXCLAMATION).
+Sources:
+
+I, Andrew Achusim, made the following modifications:
+(Mark with an [X] every modification you did. Leave the rest blank [].)
+	[X] STOP ON EXCLAMATION
+	[X] STOP ON EXCLAMATION TIDIED
+	[] QUIT TO EXIT
+	[] BREAK BETWEEN WORDS
+	[] MULTIWORD MATCH
+	[] THREE BUTTON OUTPUT
+*/
+public class Main
+{
+	public static void main(String[] args)
+	{
+		SuggestionGUI application = new SuggestionGUI();
+	}
+}
